4e001584999cb422f8272a3bd22fc7b798e72168
Stephenmuhindi <bteneson42@gmail.com>
2023-10-03 12:13:16 -0700

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        output = ""
+        if number % 3 == 0:
+            output += "Fizz"
+        if number % 5 == 0:
+            output += "Buzz"
+        if not output:
+            output = str(number)
+        print("{} ".format(output), end="")
+fizzbuzz()
