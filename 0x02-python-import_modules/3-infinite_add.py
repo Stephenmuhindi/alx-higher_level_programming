c7543f4eb075a0373b2d9f3f8f456f7a7687e601
Stephenmuhindi <bteneson42@gmail.com>
2023-10-05 13:19:05 -0700

    task 3.6

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
-def main():
+
+if __name__ == "__main__":
     arguments = sys.argv[1:]
     total = sum(int(arg) for arg in arguments)
-    print(total)
-if __name__ == "__main__":
-    main()
+    print("Total: {}".format(total))
