f722d8e52c5e0476193a336c96e9a1882a90a200
Stephenmuhindi <bteneson42@gmail.com>
2023-10-05 11:33:22 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+
+    count = len(sys.argv) - 1
+
+
+    if count == 0:
+        print("0 arguments:")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
+
+
+if len(sys.argv) == 1:
+    print(".")
