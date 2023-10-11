94d201e35bd3e4aafdbc493c1c09cef728f0d8a9
Stephenmuhindi <bteneson42@gmail.com>
2023-10-11 12:42:19 -0700

    13.2 adv

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return 0
-
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
-
+    numerator = 0
+    denominator = 0
+    for top, bottom in my_list:
+        numerator += (top * bottom)
+        denominator += bottom
+    return numerator / denominator
