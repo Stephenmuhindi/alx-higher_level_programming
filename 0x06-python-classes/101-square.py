1b4bf1ccd46de4a725a32b76922f34ac63e46327
Stephenmuhindi <bteneson42@gmail.com>
2023-10-24 16:11:35 -0700

    ni ya leo

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""this is a square class"""
+
+
+class Square():
+    """Defines modules attributes and classes."""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple and len(value) == 2 and \
+            type(value[0]) is int and type(value[1]) is int and \
+                value[0] >= 0 and value[1] >= 0:
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size > 0:
+            for y in range(self.__position[1]):
+                print()
+            for x in range(self.__size):
+                print(' ' * self.__position[0], end='')
+                print('#' * self.__size)
+        else:
+            print()
+
+    def __str__(self):
+        """ minus trailing newline """
+        length = len(self.str_format())
+        truncated = self.str_format()[:length - 1]
+        return truncated
