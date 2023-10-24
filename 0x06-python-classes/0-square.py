d4b56261f2da7b1921b1d7238ae22c31716f6d95
Stephenmuhindi <bteneson42@gmail.com>
2023-10-24 10:31:30 -0700

    task 0.3

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """
-    This is the Square class
-    """
+    This defines Square class
+"""
+
+
 class Square:
-   """ You can use this class to represent a square shape.
-    It has no attributes or methods"""
+    """
+    You can use this class to represent a square shape.
+    It has no attributes or methods
+    """
     pass
