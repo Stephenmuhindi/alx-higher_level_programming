f41a946b8e7e703d5afacfdcded580c6af3b7d27
Stephenmuhindi <bteneson42@gmail.com>
2023-10-24 11:54:06 -0700

    task 4.0

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""this is a square class"""
+
+
+class Square:
+    """defined methodand exceptions"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size * self.__size
