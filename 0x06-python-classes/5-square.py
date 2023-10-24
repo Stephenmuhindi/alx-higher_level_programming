e06787351fbf46f007df472337d196f21dfc9e03
Stephenmuhindi <bteneson42@gmail.com>
2023-10-24 12:07:21 -0700

    task 5.0

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""this is a square class"""
+
+
+class Square:
+    """defined methodand exceptions"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size > 0:
+            for x in range(self.__size):
+                print('#' * self.__size)
+        else:
+            print()
