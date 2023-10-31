c69cc1b7807bf884a39e79604bb3d50484683a32
Stephenmuhindi <bteneson42@gmail.com>
2023-10-31 04:59:11 -0700

    task adv

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-magic_string = __import__('100-magic_string').magic_string
-for x in range(10):
-    print(magic_string())
+def magic_string(string=[]):
+    string += ["BestSchool"]
+    return ", ".join(string)
