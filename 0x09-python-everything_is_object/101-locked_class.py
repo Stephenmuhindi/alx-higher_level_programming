d1aa581566d95905f04f1558ceee017ea1de539a
Stephenmuhindi <bteneson42@gmail.com>
2023-10-31 05:13:36 -0700

    task 2

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,7 +3,12 @@
 locked class def
 """
 
+
 class LockedClass:
+    """
+    methods and instances
+    """
     __slots__ = ["first_name"]
+
     def __init__(self):
         self.first_name = "first_name"
