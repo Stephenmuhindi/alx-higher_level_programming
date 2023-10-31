f189b978d816769545c9fe3d78326382cda7ac14
Stephenmuhindi <bteneson42@gmail.com>
2023-10-31 13:11:13 -0700

    task7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    numpy as np matrix multiplication
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    returns * matrix
+    """
+
+    result = np.dot(m_a, m_b)
+    return result
