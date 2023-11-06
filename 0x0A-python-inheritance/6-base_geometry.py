a2cfc3421ccf56cdabc83756bca99e1788b22ed0
Stephenmuhindi <bteneson42@gmail.com>
2023-11-06 13:57:32 -0800

    6.1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+instance def
+"""
+
+
+class BaseGeometry(object):
+    """
+    baseseo def
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
