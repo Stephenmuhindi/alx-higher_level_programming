862e90a31941589cbee1df2dd157836ff495dcbe
Stephenmuhindi <bteneson42@gmail.com>
2023-11-06 17:21:23 -0800

    awe

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-
+"""
+func decrptn
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-"""
-rectangle sumbufu
-"""
 
 
 class Rectangle(BaseGeometry):
     """
-    Rectangle def
+    shape
     """
+
     def __init__(self, width, height):
+        """
+        creates one rect
+        height
+        width
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
