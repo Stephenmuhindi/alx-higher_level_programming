7ee3a1619b980bc25c5c82bda490554d7f26b24b
Stephenmuhindi <bteneson42@gmail.com>
2023-11-07 09:34:11 -0800

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+mod def
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    scans and displays content
+    """
+
+    lines = 0
+    with open(filename) as f:
+        for lin in f:
+            lines += 1
+        f.seek(0)
+        if nb_lines <= 0 or nb_lines >= lines:
+            print(f.read(), end="")
+        else:
+            for i in range(0, nb_lines):
+                print(f.readline(), end="")
