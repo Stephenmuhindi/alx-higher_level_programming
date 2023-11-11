530540686595c88f328f8333299680a6d0669df7
Stephenmuhindi <bteneson42@gmail.com>
2023-11-10 16:17:26 -0800

    wueh 16

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" mod def"""
+from json import dumps, loads
+import csv
+
+
+class Base:
+    """zero"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ init"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return dumps(list_dictionaries)
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""mod def"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class def"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """init"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str """
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """ update"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """ class dict"""
+        return {"id": self.id, "x": self.x,
+                "size": self.width, "y": self.y}
