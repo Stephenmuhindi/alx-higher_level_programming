352beecdee25cd1ebcc70427dcd8e3ee44fcde04
Stephenmuhindi <bteneson42@gmail.com>
2023-12-13 12:02:51 -0800

    aty

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,2 @@
 -- displays the top 3 of cities temperature during July and August
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
