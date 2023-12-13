ff2aeac2a6ff7af9f84816b58ca639581c6cdc35
Stephenmuhindi <bteneson42@gmail.com>
2023-12-13 09:51:11 -0800

    100.2

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
---alters the character set and collation for the entire database named hbtn_0c_0 to use utf8mb4 character set
+-- alters the character set and collation for the entire database named hbtn_0c_0 to use utf8mb4 character set
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
