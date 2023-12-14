24dadd0dda998a745ce440a2b530f24ad2de32ef
Stephenmuhindi <bteneson42@gmail.com>
2023-12-14 11:09:34 -0800

    all

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities contained
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states on cities.state_id = states.id
+ORDER BY cities.id ASC;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
