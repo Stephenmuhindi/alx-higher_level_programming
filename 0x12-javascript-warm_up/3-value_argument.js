106386211dfcd2d5acd472c0a17c8621138257db
Stephenmuhindi <bteneson42@gmail.com>
2024-01-08 10:13:47 +0000

    task 3.1

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+/*
+the first argument passed to it
+is printed
+*/
 const arg = process.argv[2];
-if (arg === undefined)
-{
+if (arg === undefined) {
   console.log('No argument');
-}
-else
-{
+} else {
   console.log(arg);
 }
