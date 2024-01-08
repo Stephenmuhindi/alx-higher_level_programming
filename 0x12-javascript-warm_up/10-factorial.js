89f2ca9ef4d64a4607f6cca7eb996709565a93cd
Stephenmuhindi <bteneson42@gmail.com>
2024-01-08 10:52:22 +0000

    10.1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 /*
 computes and prints a factorial
 
-The first argument is integer 
+The first argument is integer
 (argument can be cast as integer)
 used for computing the factorial
 Factorial of NaN is 1
