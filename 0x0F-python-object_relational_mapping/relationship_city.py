0ba7a8b5e767e18d56771a3e7a471eef636942ec
Stephenmuhindi <bteneson42@gmail.com>
2024-01-13 11:54:30 -0800

    kuli

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+relationship tableau for cities using decla
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_city import City
+
+
+class City(Base):
+    """tableau de cities"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
