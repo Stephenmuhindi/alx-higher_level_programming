96463a842b462868ef0240685dd5ae7a88f56d43
Stephenmuhindi <bteneson42@gmail.com>
2024-01-13 10:09:31 -0800

    diddit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,6 @@ Base = declarative_base()
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from model_city import City
 
 Base = declarative_base()
 
